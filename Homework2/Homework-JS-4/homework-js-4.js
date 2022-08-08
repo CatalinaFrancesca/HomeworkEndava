bf5bcb251c54f94ed3c9032abe546dcd6c29b830
FrancescaPripis <francesca.pripis@yahoo.com>
2022-08-08 19:50:20 +0300

    - exercise 4 done

---
+++ Homework2/Homework-JS-4/homework-js-4.js
@@ -0,0 +1,17 @@
+function countElementsInDocument(document) {
+  let div = document.getElementById("divId");
+  let list = div.querySelectorAll("text");
+  let elementNumber = 0;
+  if (elementNumber) {
+    elementNumber +=
+      document.getElementById("elementName").innerHTML + list.length;
+  } else {
+    elementNumber =
+      document.getElementById("elementName").innerHTML + list.length;
+  }
+  //document.getElementById("elementNumber").innerHTML=list.length;
+  //document.getElementById("elementNumber").innerHTML=list.childElementCount;
+  //console.log(list.getAttribute("p"));
+  console.log(elementNumber);
+}
+countElementsInDocument(document);
